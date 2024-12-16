69b6555d54d5c9e2a092944c4f5457f232486e52
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-16 16:08:04 -0300

    Mais desafio resolvido e melhoria
    Adicionando melhoria no desafio 003

--- desafio/003.py
+++ desafio/003.py
@@ -1,3 +1,5 @@
 n1 = int(input('Digite um número : '))
 n2 = int(input('Digite outro número : '))
 print('A soma de', n1, 'e', n2, '=', n1 + n2)
+
+print('A soma de {} + {} = {}'.format(n1, n2, n1 + n2))
