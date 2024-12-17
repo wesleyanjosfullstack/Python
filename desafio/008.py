d313757a3f0b13d86621a562248d5cfd8d870a32
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-17 12:14:55 -0300

    Melhorando desafio 008 com mais medidas

--- desafio/008.py
+++ desafio/008.py
@@ -1,4 +1,15 @@
 m = float(input('Digite um valor em metros: '))
-c = m * 100
+print('=' * 30)
+print('Médida de {:.1f}m corresponde á:'.format(m))
+print('=' * 30)
+
+cm = m * 100
 mm = m * 1000
-print('Metros: {:.2f}m\nCentimetros: {:.0f}cm\nMilimetros: {:.0f}mm'.format(m, c, mm))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+
+print('Quilometros: {:.3f}km\nHectômetro: {:.2f}hm'.format(km, hm))
+print('Decâmetro: {:.1f}dam\nDecímetro: {:.0f}dm'.format(dam, dm))
+print('Centimetros: {:.0f}cm\nMilimetros: {:.0f}mm\n'.format(cm, mm))
