e40135ff17cb3e6cf11c32c04f654f365158b7c2
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-18 16:43:03 -0300

    Corrigindo desafio 023

--- desafio/023.py
+++ desafio/023.py
@@ -1,4 +1,12 @@
-v = str(input('Digite um valor entre 0 a 9999: '))
-print('Valor diitado {}'.format(v))
-u = v.split()
-print('Unidade: {}'.format(u[1:2]))
+number = int(input('Digite um valor entre 0 a 9999: '))
+print('\nAnálisando o número {}...\n'.format(number))
+
+u = number // 1 % 10
+d = number // 10 % 10
+c = number // 100 % 10
+m = number // 1000 % 10
+
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('milhar: {}'.format(m))
