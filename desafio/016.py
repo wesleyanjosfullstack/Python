2b22c01261dd9c521cd7891926c56966f62aed5a
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-18 10:22:45 -0300

    Alterando desafio 016
    Modulo math com função trunc

--- desafio/016.py
+++ desafio/016.py
@@ -1,3 +1,3 @@
-import math
+from math import trunc
 number = float(input('Digite um valor real: '))
-print('O valor de {} inteiro é {}\n'.format(number, math.floor(number)))
+print('O valor de {} inteiro é {}\n'.format(number, trunc(number)))
