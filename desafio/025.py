63196c6d4083feda74201b5b6ce504aa0f183f04
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-18 16:56:55 -0300

    ALternado desafio 025
    Utilizando in para encontrar silva no nome digitado

--- desafio/025.py
+++ desafio/025.py
@@ -1,7 +1,3 @@
-name = str(input('Digite seu nome: '))
-
-print('Seu nome é {}'.format(name))
-
-s = name.split()
-
-print('Tem Silva ? {}'.format(s[:].upper().find('silva')))
+name = str(input('Digite seu nome: ')).strip().upper()
+print('\nAnálisando o nome {}...\n'.format(name))
+print('Seu nome tem Silva ? {}'.format('SILVA' in name))
