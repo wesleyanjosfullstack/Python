f193dc173bd4f5be5de589c41656cfdcba9528bd
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-19 10:41:05 -0300

    Melhorando e ajustando desafio 035

--- desafio/035.py
+++ desafio/035.py
@@ -1,10 +1,14 @@
-l1 = int(input('Digite o primeiro comprimento: '))
-l2 = int(input('Digite o segundo comprimento: '))
-l3 = int(input('Digite o terceiro comprimento: '))
+print('=' * 30)
+print('Análisando de Triângulo')
+print('=' * 30)
+
+l1 = float(input('Digite o primeiro comprimento: '))
+l2 = float(input('Digite o segundo comprimento: '))
+l3 = float(input('Digite o terceiro comprimento: '))
+
 print('\nAnálisando os comprimentos: {}, {} e {}...\n'.format(l1, l2, l3))
 
-if l1 == l2 and l1 == l3:
-    print('Podem forma um triângulo equilatero\n')
+if l1 < l2 + l3 and l2 < l1 + l3 and l3 < l1 + l2:
+    print('Podem forma um triângulo\n')
 else:
-    if l1 != l2 and l1 != l3 and l2 != l3:
-        print('Não pode forma um triângulo !\n')
+    print('Não podem forma um triângulo\n')
