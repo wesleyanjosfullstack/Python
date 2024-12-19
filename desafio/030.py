ca49b329c38c812f16611e6c3d5f76c1fbf643d4
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-18 23:19:58 -0300

    Desafio 030 impar par

---
+++ desafio/030.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+print('\nAnálisando o valor {}...\n'.format(n))
+
+if n % 2 == 0:
+    print('Seu número é par !\n')
+else:
+    print('Seu número é impar !\n')
